9714b061f9f719893ca8049b00992a5c4e80e5db
nikiOstrov <80963098+nikiOstrov@users.noreply.github.com>
2021-11-05 15:49:38 +0100

    Enhanced bash file

--- publishTutorials.sh
+++ publishTutorials.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+# start after the folder playbooks is created in the current repository
 cd playbooks
 SPECIFIED_TUTORIALS=()
 for dir in */; do SPECIFIED_TUTORIALS+=("${dir::-1}"); done
 echo "SPECIFIED_TUTORIALS:\n"
 printf "%s\n" "${SPECIFIED_TUTORIALS[@]}"
-
-cd ../repo
+# switches to tutorials repository https://github.com/devonfw-tutorials/tutorials
+cd ../repo/tutorials
 ONLINE_TUTORIALS=()
 for dir in */; do ONLINE_TUTORIALS+=("${dir::-1}"); done
 echo "ONLINE_TUTORIALS:\n"
@@ -21,23 +21,27 @@ do
   fi
 done
 
+# switches to tutorials compiler repository https://github.com/devonfw-tutorials/tutorial-compiler (build/output/wiki)
 cd ../build/output/wiki
 GENERATED_TUTORIALS=()
 for dir in */; do GENERATED_TUTORIALS+=("${dir::-1}"); done
 echo "GENERATED_TUTORIALS:\n"
 printf "%s\n" "${GENERATED_TUTORIALS[@]}"
+# back to tutorials compiler repository
 cd ../../..
 
 # delete tutorials, which are newly generated
 for tutorial in "${ONLINE_TUTORIALS[@]}"
 do
   if [[ ! " ${GENERATED_TUTORIALS[@]} " =~ " ${tutorial} " ]]; then
-      rm -rf repo/'${tutorial}'
+      rm -rf repo/"${tutorial}"
       echo "deleted ${tutorial} as this is up to be replaced by the build"
   fi
 done
 
-cp -r build/output/wiki/*/ repo/
+mkdir -p tutorials 
+#copy all files from * in repo/tutorials (https://github.com/devonfw-tutorials/wiki-tutorials/tutorials)
+cp -r build/output/wiki/*/ repo/tutorials
 cd repo/
 git add -A
 git config user.email "devonfw"
